d83644efb64e6a52e6bd8904b4a9d717ab3710bd
Mason Brown <51417315+itsmasoncb@users.noreply.github.com>
2024-07-02 17:26:16 -0400

    updated README.md. Updated az-parse-v2.py, script now can take a wishlist URL, and return validated ISBN10 and ISBN13 lists.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # az-overdrive
-Export amazon wishlist, parse, and use Libby API to search for books. Borrow instead of buy.
+Export amazon wishlist, parse ISBNs/ASINs, and use Libby API/Library Resources to search for books. Borrow instead of buy.
+
+## Current State - 07.02.2024
+az-parse-v2.py can take an amazon wishlist of books and return the ISBN10 and ISBN13 values in a list.
+
+## Future State
+Future updates will include automatic search across library resources via returned ISBNs and a yet undetermined method to filter out non-books and search audiobooks as well. 
--- az-parse-v2.py
+++ az-parse-v2.py
@@ -2,93 +2,178 @@
 # Import Setup
 ##########################
 import logging
-import coloredlogs
-import requests
-from bs4 import BeautifulSoup
+import os
 from time import sleep
-from validator_collection import checkers
+
+from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver import Keys
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.common.by import By
+from validator_collection import checkers
 
 ##########################
 # Variable Setup
 ##########################
 logger = logging.getLogger('MasonWishList')
 logging.basicConfig(level=logging.INFO)
+os.environ['SE_AVOID_STATS'] = 'true'
 
 wishlist_url = 'https://www.amazon.com/hz/wishlist/ls/36B9FRG2AYASB'
 overdrive_url = 'https://www.overdrive.com/search?q='
 libbyapp_url = 'https://share.libbyapp.com/title/'
 
 
-def asin_to_isbn(asin: str):
-    """
+def setup_driver():
+    # Setup Selenium WebDriver
+    options = ChromeOptions()
+    options.add_argument('--headless')
+    options.add_argument('--start-maximized')
+    options.add_argument('--disable-dev-shm-usage')
 
-    :param asin:
-    :return:
+    driver = webdriver.Chrome(options=options)
+    logger.info('Returning driver settings, zoom zoom.')
+    return driver
+
+
+def lazy_load_wishlist(driver):
+    """
+    """
+    try:
+        logger.info('Lazy Loading the webpage, this may take a moment...')
+
+        while True:
+            # Scroll to the bottom of the page
+            driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
+            sleep(5)  # Wait for content to load
+
+            # Parse the page content with BeautifulSoup
+            soup = BeautifulSoup(driver.page_source, 'html.parser')
+
+            # Check if "End of list" is in the page
+            end_of_list = soup.find('h5', {'aria-level': '5'}, string='End of list')
+            if end_of_list:
+                logging.info("Found the end of the wishlist. Good job.")
+                return soup
+
+    except BaseException as e:
+        logger.error('lazy_load_wishlist has encountered an error. The error reads: {}'.format(e))
+        exit(1)
+
+
+def extract_wishlist_urls(soup):
+    """
+    """
+    item_urls = []
+    logger.info('Extracting URLs from HTML wishlist data...')
+    try:
+        # Extract item URLs
+        for item in soup.select("a.a-link-normal"):
+            href = item.get('href')
+            if href and '/dp/' in href:
+                item_url = "https://www.amazon.com" + href.split('?')[0]
+                if item_url not in item_urls:
+                    item_urls.append(item_url)
+
+        return item_urls
+
+    except BaseException as e:
+        logger.error('extract_wishlist_urls has encountered an error. The error reads: {}'.format(e))
+        exit(1)
+
+
+def clean_urls(urls):
+    cleaned_urls = []
+    invalid_urls = []
+    for url in urls:
+        try:
+            # Extract the part of the URL between the first and second slashes after the domain
+            amazon_product_code = url.split('/')[4]
+            # Remove last character as valid ISBN10 can contain X as the last digit.
+            amazon_product_code_int = amazon_product_code[:-1]
+            if checkers.is_integer(amazon_product_code_int):
+                cleaned_urls.append(amazon_product_code)
+            else:
+                logging.info('Invalid ISBN, this may be an audiobook: {}'.format(amazon_product_code))
+                invalid_urls.append(amazon_product_code)
+
+        except BaseException as e:
+            logger.error('clean_urls has encountered an error. The error reads: {}'.format(e))
+            exit(1)
+
+    return cleaned_urls, invalid_urls
+
+
+def asin_to_isbn(asin_list: list):
+    """
     """
     logger.info('Converting ASIN to ISBN..')
-    checkers.is_integer(asin)
-
+    isbn_10_list = []
+    isbn_13_list = []
     try:
-        if len(str(asin)) == 10:
-            isbn_10 = asin
-            logger.info('ISBN10 calculated: {}'.format(isbn_10))
-        else:
-            isbn_10 = None
-        checkers.has_length(asin, minimum=10, maximum=13)
-        # 978 Prefix, minus last digit for check digit calculation
-        isbn_13_no_check_digit = '978' + asin[:-1]
-        logger.info('Intermediate 12 digit code created')
-        total = 0
-        for index, digit in enumerate(isbn_13_no_check_digit):
-            if index % 2 == 0:
-                total += int(digit) * 1
-            else:
-                total += int(digit) * 3
-        remainder = total % 10
-        check_digit = (10 - remainder) % 10
-        isbn_13 = isbn_13_no_check_digit + str(check_digit)
-        logger.info('ISBN-13 calculated: {}'.format(isbn_13))
+        for asin in asin_list:
+            if checkers.has_length(asin, minimum=10, maximum=10):
+                isbn_10 = asin
+                logger.info('ISBN10 validated and returned: {}'.format(isbn_10))
+                isbn_10_list.append(isbn_10)
 
-        return isbn_10, isbn_13
+        for isbn_13 in isbn_10_list:
+            # 978 Prefix, minus last digit for check digit calculation
+            isbn_13_no_check_digit = '978' + isbn_13[:-1]
+            total = 0
+
+            # This math is confusing but end results find the book with both numbers correctly
+            for index, digit in enumerate(isbn_13_no_check_digit):
+                if index % 2 == 0:
+                    total += int(digit) * 1
+                else:
+                    total += int(digit) * 3
+            remainder = total % 10
+            check_digit = (10 - remainder) % 10
+            isbn_13_final = isbn_13_no_check_digit + str(check_digit)
+
+            checkers.has_length(isbn_13_final, minimum=13, maximum=13)
+            logger.info('ISBN-13 validated and converted: {}'.format(isbn_13_final))
+            isbn_13_list.append(isbn_13_final)
+
+        logger.info('ISBN10 and ISBN13 lists created.')
+        return isbn_10_list, isbn_13_list
 
     except BaseException as e:
         logger.error('asin_to_isbn has encountered an error. The error reads: {}'.format(e))
+        exit(1)
 
 
-def wishlist_to_urls():
-    """
+def get_amazon_wishlist_urls():
+    try:
+        driver = setup_driver()
+        driver.get(wishlist_url)
 
-    :return:
-    """
-    # TODO: Use Selenium to lazy load wishlist
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
-    }
-    response = requests.get(wishlist_url, headers=headers)
+        soup = lazy_load_wishlist(driver)
+        driver.quit()
 
-    if response.status_code != 200:
-        print("Failed to retrieve the wishlist page")
-        return []
+        item_urls = extract_wishlist_urls(soup)
+        clean_isbn_list, _ = clean_urls(item_urls)
+        final_isbn_10_list, final_isbn_13_list = asin_to_isbn(clean_isbn_list)
 
-    soup = BeautifulSoup(response.content, 'html.parser')
+        return final_isbn_10_list, final_isbn_13_list
 
-    item_urls = []
-    for item in soup.select("a.a-link-normal"):
-        href = item.get('href')
-        # Look for URLS with /dp/
-        if href and '/dp/' in href:
-            item_url = "https://www.amazon.com" + href.split('?')[0]
-            item_urls.append(item_url)
-
-    return item_urls
+    except BaseException as e:
+        logger.error('extract_wishlist_urls has encountered an error. The error reads: {}'.format(e))
+        exit(1)
 
 
+def main():
+    final_isbn_10_list, final_isbn_13_list = get_amazon_wishlist_urls()
+    # TODO: Add library search functionality.
+    try:
+        logger.info(final_isbn_10_list)
+        logger.info(final_isbn_13_list)
+
+    except BaseException as e:
+        logger.error('main has encountered an error. The error reads: {}'.format(e))
+        exit(1)
 
 
-# asin = '0063347539'
-# isbn = asin_to_isbn(asin)
-# if isbn:
-#     print(f"The ISBN for ASIN {asin} is {isbn}.")
-# else:
-#     print("Invalid ASIN. It must be a numeric string.")
+if __name__ == "__main__":
+    main()
